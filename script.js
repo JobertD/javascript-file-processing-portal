652d7185c2afd07796131af846e2ac552efa0dd4
JobertD <2226374@slu.edu.ph>
2023-10-13 20:30:16 +0800

    changed to staff login and added staff.json

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Load and parse the JSON file containing student data
-    fetch("../students.json")
+    // Load and parse the JSON file containing staff data
+    fetch("../staff.json")
       .then((response) => response.json())
       .then((data) => {
         const loginForm = document.querySelector("form");
@@ -8,28 +8,28 @@ document.addEventListener("DOMContentLoaded", function () {
         loginForm.addEventListener("submit", function (e) {
           e.preventDefault();
   
-          // Get the entered student ID and password
-          const studentId = document.querySelector('#studentID').value;
+          // Get the entered staff ID and password
+          const staffId = document.querySelector('#staffID').value;
           const password = document.querySelector('#password').value;
   
-          // Check if the student ID and password match any entry in the JSON data
-          const student = data.students.find(
-            (student) => 
-              student.studId === studentId && student.password === password
+          // Check if the staff ID and password match any entry in the JSON data
+          const faculty = data.staff.find(
+            (staff) => 
+              staff.staffId === staffId && staff.password === password
           );
   
-          if (student) {
+          if (faculty) {
             // Successful login, you can redirect to another page or perform other actions here
             alert("Login successful!");
             // You can add code to redirect to another page here if needed.
           } else {
             // Failed login attempt
-            alert("Invalid student ID or password. Please try again.");
+            alert("Invalid staff ID or password. Please try again.");
           }
         });
       })
       .catch((error) => {
-        console.error("Error loading student data:", error);
+        console.error("Error loading staff data:", error);
       });
   });
   
