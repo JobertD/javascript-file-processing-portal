8723fd54c47860e5f66411671990c6ae5b3310a3
Ian Cristino <2224378@slu.edu.ph>
2023-10-15 19:43:08 +0800

    Display students on table //TODO: put css

--- scripts/portal.js
+++ scripts/portal.js
@@ -4,12 +4,13 @@ let fName = localStorage.getItem("userFirstName");
 let lName = localStorage.getItem("userLastName");
 let logOutLi = document.querySelector("li#log-out-li");
 let dropDown = document.querySelector("select");
+let studentTable = document.querySelector("#student-table"); // Assuming you have a table element with the id "student-table"
 
 // Add in welcome text with the user's name.
 loggedInText.textContent = `Welcome, ${fName} ${lName}!`;
 
 logOutLi.addEventListener("click", function () {
-    // When the user logs out, remove references of the user's name in localstorage.
+    // When the user logs out, remove references of the user's name in local storage.
     localStorage.removeItem("userFirstName");
     localStorage.removeItem("userLastName");
     localStorage.removeItem("staffId"); // Change "userId" to "staffId"
@@ -43,4 +44,33 @@ Promise.all([
 })
 .catch((error) => {
     console.log("Error loading class codes or staff data: ", error);
+});
+
+
+// Event listener for dropdown selection
+dropDown.addEventListener("change", function () {
+    const selectedClassCode = dropDown.value;
+    
+    // Clear existing table rows
+    const tbody = document.querySelector("#student-table tbody");
+    while (tbody.hasChildNodes()) {
+        tbody.removeChild(tbody.firstChild);
+    }
+
+    // Fetch student data based on the selected class code and update the table
+    fetch("/data/students.json") // Replace with the actual path to your students JSON file
+        .then(response => response.json())
+        .then(data => {
+            data.students.forEach(student => {
+                if (student.classCode === selectedClassCode) {
+                    const newRow = tbody.insertRow(-1);
+                    const cell1 = newRow.insertCell(0);
+                    const fullName = `${student.fName} ${student.lName}`;
+                    cell1.textContent = fullName;
+                }
+            });
+        })
+        .catch(error => {
+            console.log("Error loading student data: ", error);
+        });
 });
