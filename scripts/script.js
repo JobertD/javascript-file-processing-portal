1e17d8ddc62ecd2282601e74eb502f3b5241d449
Yustine Arevalo <2225602@slu.edu.ph>
2023-10-15 12:56:23 +0800

    Sort Files and Add redirect to portal page

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
     // Load and parse the JSON file containing staff data
-    fetch("../staff.json")
+    fetch("/data/staff.json")
       .then((response) => response.json())
       .then((data) => {
         const loginForm = document.querySelector("form");
@@ -22,6 +22,12 @@ document.addEventListener("DOMContentLoaded", function () {
             // Successful login, you can redirect to another page or perform other actions here
             alert("Login successful!");
             // You can add code to redirect to another page here if needed.
+
+            //Remember the account details and redirect to the page for the portal.
+            localStorage.setItem("userId", staffId);
+            location.replace("/pages/portal.html");
+
+
           } else {
             // Failed login attempt
             alert("Invalid staff ID or password. Please try again.");
